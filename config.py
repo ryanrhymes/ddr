4c1d743af0776c984c5b26bfac2a6a321306dfa2
Liang Wang <ryanrhymes@gmail.com>
2015-06-15 20:10:48 +0100

    edit ...

---
+++ config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Config contains the basic parameters of DDR system.
+#
+# Liang Wang @ Computer Lab, Cambridge University
+# 2015.06.15
+
+
+class Config():
+
+    def __init__(self, quota):
+        """Init a config object"""
+        pass
+
+    pass
